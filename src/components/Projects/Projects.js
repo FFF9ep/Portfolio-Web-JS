927c75e04d208e6b0dbe2b5f86d172b89fed9c4f
fandi a <fandiiardy@gmail.com>
2025-01-02 20:20:08 +0700

    update project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -48,9 +48,9 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage."
               ghLink=" "
-              demoLink=" "              
+              demoLink=" "
             />
           </Col>
 
