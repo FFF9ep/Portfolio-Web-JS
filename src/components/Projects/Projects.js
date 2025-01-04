3368bc483d7f4f0ecec4b9a65e05e05e1f4190e1
fandi a <fandiiardy@gmail.com>
2025-01-04 17:51:37 +0700

    update project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -82,7 +82,7 @@ function Projects() {
               isBlog={false}
               title="Face Recognition and Emotion Detection!"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion."
               ghLink=" "
               demoLink=" "
             />
